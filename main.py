2ceafdce008bdea23032ab360f51fe2e309b9080
emmittjames <emmittjames1@gmail.com>
2024-04-29 16:00:06 -0400

    cors middleware

--- main.py
+++ main.py
@@ -1,54 +1,18 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
-    weather_data = {
-        "coord": {
-            "lon": -0.1257,
-            "lat": 51.5085
-        },
-        "weather": [
-            {
-                "id": 803,
-                "main": "Clouds",
-                "description": "broken clouds",
-                "icon": "04n"
-            }
-        ],
-        "base": "stations",
-        "main": {
-            "temp": 285.48,
-            "feels_like": 284.44,
-            "temp_min": 283.81,
-            "temp_max": 286.23,
-            "pressure": 1016,
-            "humidity": 64
-        },
-        "visibility": 10000,
-        "wind": {
-            "speed": 3.09,
-            "deg": 170
-        },
-        "clouds": {
-            "all": 53
-        },
-        "dt": 1714419471,
-        "sys": {
-            "type": 2,
-            "id": 2091269,
-            "country": "GB",
-            "sunrise": 1714365321,
-            "sunset": 1714418402
-        },
-        "timezone": 3600,
-        "id": 2643743,
-        "name": "London",
-        "cod": 200
-    }
-
-    return weather_data
+    return {"message": "Hello World"}
